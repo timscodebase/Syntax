3b895cd53baa6022d4bacbd962c19658fa12008f
Tim Smith <gonad2019@gmail.com>
2021-11-10 12:35:50 -0800

    init

---
+++ src/components/Player.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let src
+  let playbackRate = 1
+</script>
+
+<audio bind:playbackRate id="audio" {src} controls></audio>
+<select bind:value={playbackRate} name="playback" id="playback">
+  <option value={1}>1</option>
+  <option value={1.5}>1.5</option>
+  <option value={2}>2</option>
+</select>
